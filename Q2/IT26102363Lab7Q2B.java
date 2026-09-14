7b6947fe62d177668998ec95d45a666d4234ccfe
IT26102363 <It26102363@my.sliit.lk>
2026-09-14 21:14:08 +0530

    Add files via upload

---
+++ Q2/IT26102363Lab7Q2B.java
@@ -0,0 +1,16 @@
+public class  IT26102363Lab7Q2B {
+
+    public static void main(String[] args) {
+		
+		for(int i=1; i<=5; i++){
+			
+			System.out.print(i + " - ");
+			
+			for(int a=1; a<=i; a++){
+				System.out.print("* ");
+			}
+			
+			System.out.println();
+		}
+	}
+}
---
+++ Q2/IT26102363Lab7Q2C.java
@@ -0,0 +1,15 @@
+public class  IT26102363Lab7Q2C {
+
+    public static void main(String[] args) {
+		
+		for(int i=5; i>=1; i--){
+			
+			
+			for(int a=1; a<=i; a++){
+				System.out.print(i);
+			}
+			
+			System.out.println();
+		}
+	}
+}
